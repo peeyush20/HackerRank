6e7d24099a09b1a2aaa50f259eb67ce48e67c7e6
peeyush20 <jainpeeyush20@gmail.com>
2018-03-25 13:00:46 +0530

    Create Data Types.py

---
+++ Data Types.py
@@ -0,0 +1,15 @@
+
+# Declare second integer, double, and String variables.
+num= int(input())
+flt = float(input())
+st = input()
+
+# Print the sum of both integer variables on a new line.
+print(i + num)
+
+# Print the sum of the double variables on a new line.
+print(d + flt)
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s+st)
